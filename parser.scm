8d9a229c9a948ba1fda8588c8667b7eeb068ea90
Cedrice Ermineii <CedriceRmineii@protonmail.com>
2022-01-01 14:18:28 +0000

    reformat parser for indentation consistency

--- parser.scm
+++ parser.scm
@@ -31,34 +31,46 @@
    (expect: 1)
    ;; terminal token types
    (open-paren close-paren name-token assign double-colon colon character dot
-               string-token number dollar-sign)
-   (expression (double-colon names) : `(module ,$2)
-               (parameters scope) : (if (null? $1) $2 `(parameters ,$1 ,$2)))
-   (names (name colon names) : (cons $1 $3)
-          (name) : (list $1))
-   (parameters (colon name-token parameters) : (cons $2 $3)
-               () : '())
-   (scope (subexpression assign name-token scope) : `(assign ,$1 ,$3 ,$4)
-          (subexpression) : $1)
-   (subexpression (value applications) : `(sub-expr ,$1 ,$2)
-                  (applications) : `(sub-expr (delay (name () ,implicit-value))
-                                              ,$1))
-   (applications (application applications) : (cons $1 $2)
-                 () : '())
-   (application (function arguments) : `(application ,$1 ,$2))
-   (function (open-paren expression close-paren) : $2
-             (dollar-sign expression) : $2
-             (name) : $1)
-   (arguments (argument arguments) : (cons $1 $2)
-              () : '())
-   (argument (colon function) : `(sub-expr (delay (name () ,implicit-value))
-                                           ((application ,$2 ())))
-             (colon value) : $2
-             (value) : $1)
-   (name (qualifiers name-token) : `(name ,$1 ,$2))
-   (qualifiers (qualifiers name-token double-colon) : (cons $2 $1)
-               () : '())
-   (value (number) : $1
-          (string-token) : $1
-          (dot function) : `(delay ,$2)
-          (character) : $1)))
+    string-token number dollar-sign)
+   (expression
+     (double-colon names) : `(module ,$2)
+     (parameters scope) : (if (null? $1) $2 `(parameters ,$1 ,$2)))
+   (names
+     (name colon names) : (cons $1 $3)
+     (name) : (list $1))
+   (parameters
+     (colon name-token parameters) : (cons $2 $3)
+     () : '())
+   (scope
+     (subexpression assign name-token scope) : `(assign ,$1 ,$3 ,$4)
+     (subexpression) : $1)
+   (subexpression
+     (value applications) : `(sub-expr ,$1 ,$2)
+     (applications) : `(sub-expr (delay (name () ,implicit-value)) ,$1))
+   (applications
+     (application applications) : (cons $1 $2)
+     () : '())
+   (application
+     (function arguments) : `(application ,$1 ,$2))
+   (function
+     (open-paren expression close-paren) : $2
+     (dollar-sign expression) : $2
+     (name) : $1)
+   (arguments
+     (argument arguments) : (cons $1 $2)
+     () : '())
+   (argument
+     (colon function) : `(sub-expr (delay (name () ,implicit-value))
+                                   ((application ,$2 ())))
+     (colon value) : $2
+     (value) : $1)
+   (name
+     (qualifiers name-token) : `(name ,$1 ,$2))
+   (qualifiers
+     (qualifiers name-token double-colon) : (cons $2 $1)
+     () : '())
+   (value
+     (number) : $1
+     (string-token) : $1
+     (dot function) : `(delay ,$2)
+     (character) : $1)))
