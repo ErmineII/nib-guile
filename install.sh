af6609f544bc362dedd4eb5a7ca3f1803dc546fe
Cedrice Ermineii <CedriceRmineii@protonmail.com>
2022-01-03 22:26:01 +0000

    automatically compile nib when installing it

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ guile_site_dir="$(guile -c '(display (%site-dir))')"
 echo "Making directory $guile_site_dir/language"
 mkdir -p "$guile_site_dir/language"
 ln -vs "$PWD" "$guile_site_dir/language/nib"
+
+# test that things are working and compile files
+guile --auto-compile -c '(use-modules (language nib spec))'
